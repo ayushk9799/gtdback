ccef34c4447f5598de9ba3a7f44a06afe86292bc
AYUSH KUMAR <ayushkumar@AYUSHs-MacBook-Air.local>
2025-10-14 11:12:16 +0530

    chnages

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -3,27 +3,37 @@ import { OAuth2Client } from "google-auth-library";
 import User from "../models/User.js";
 
 const router = Router();
-const client = new OAuth2Client(
-  "125181194595-uautevfk4s33h57gi28hougs7lruet70.apps.googleusercontent.com"
-);
+
 
 // Google authentication route
 router.post("/google/loginSignUp", async (req, res) => {
   try {
-    const { token } = req.body;
+    const { token , platfrom} = req.body;
 
     if (!token) {
       return res.status(400).json({ error: "Token is required" });
     }
-
+    let client;
+    if (platfrom === "android") {
+       client = new OAuth2Client(
+        "125181194595-uautevfk4s33h57gi28hougs7lruet70.apps.googleusercontent.com"
+      );
+    } else {
+       client = new OAuth2Client(
+        "125181194595-joc9v9367fldq9qigu2bh9uoosq4u67d.apps.googleusercontent.com"
+      );
+    }
+    
     // Verify the token
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience:
-        "125181194595-uautevfk4s33h57gi28hougs7lruet70.apps.googleusercontent.com",
+        platfrom === "android" ? "125181194595-uautevfk4s33h57gi28hougs7lruet70.apps.googleusercontent.com" : "125181194595-joc9v9367fldq9qigu2bh9uoosq4u67d.apps.googleusercontent.com",
     });
+    console.log("ticket", ticket);
 
     const payload = ticket.getPayload();
+    console.log("payload", payload);
 
     // Check if user exists
     let user = await User.findOne({ email: payload.email });
@@ -45,7 +55,7 @@ router.post("/google/loginSignUp", async (req, res) => {
       },
     });
   } catch (error) {
-    console.error("Error verifying Google token:", error);
+    console.error("Error verifying Google token:", error.message);
     res.status(401).json({
       success: false,
       error: "Invalid token",
